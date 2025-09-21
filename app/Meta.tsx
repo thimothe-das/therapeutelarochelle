0b57ce2ae2afbf41dab8ee74308cf9f84da24e5e
tdas <tdas@zebestof.com>
2025-09-21 23:06:01 +0200

    SEO improvments (sitemap, robots.txt canonical link)

--- app/Meta.tsx
+++ app/Meta.tsx
@@ -12,6 +12,7 @@ async function Meta() {
         name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1"
       />
+      <link rel="canonical" href="https://therapeutelarochelle.fr/" />
     </Head>
   );
 }
